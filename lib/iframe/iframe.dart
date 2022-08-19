07141fab0958843c20fa2dd401760dc91315ac30
AlvinChrist <alvinardiansyah2002@gmail.com>
2022-08-19 23:28:41 +0700

    hapus comment

--- lib/iframe/iframe.dart
+++ lib/iframe/iframe.dart
@@ -2,6 +2,5 @@ String redrawMaps(double lat, double long) {
   return """
     let iframe = document.getElementById('embed-maps');
     iframe.setAttribute('src', `https://maps.google.com/maps?q=${lat},${long}&z=15&output=embed`)
-    // el.attributes.src = `https://maps.google.com/maps?q=${lat},${long}&z=10&output=embed`
   """;
 }
