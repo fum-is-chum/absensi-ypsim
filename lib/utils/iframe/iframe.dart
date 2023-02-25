22c68200955e913e98546d20a69bc173de677f33
Alvin Christ <alvinardiansyah2002@gmail.com>
2023-02-25 16:11:56 +0700

    bugfix check isSaturday

--- lib/utils/iframe/iframe.dart
+++ lib/utils/iframe/iframe.dart
@@ -1,38 +1,5 @@
 import 'package:geolocator/geolocator.dart';
 
-String redrawMaps(
-    double lat1, double long1, double lat2, double long2, int radius) {
-  // return """
-  //   zoom = 19 - ratios.findIndex((d) => d >= D*6);
-  //   D = distanceMeter(x1,y1,x2,y2) * 2;
-  //   iframe.setAttribute('src', 'https://maps.google.com/maps?q=$lat1,$long1&z=' + zoom + '&output=embed');
-  //   document.querySelectorAll('canvas').forEach((node) => node.remove());
-  //   x2 = $lat2;
-  //   y2 = $long2;
-  //   newPoint(x2, y2, radius);
-  // """;
-  return """
-    x1 = $lat1;
-    y1 = $long1;
-
-    x2 = $lat2;
-    y2 = $long2;
-
-    radius = $radius * 2;
-
-    D = distanceMeter(x1,y1,x2,y2) * 2;
-    zoom = 19 - ratios.findIndex((d) => {
-      return d >= D*6
-    });
-
-    iframe.setAttribute('src', 'https://maps.google.com/maps?q=' + x1 + ',' + y1 + '&z=' + zoom + '&output=embed');
-    document.querySelectorAll('canvas').forEach((node) => node.remove());
-
-    newPoint(x2, y2, radius);
-    newPoint(x2, y2, 5);
-  """;
-}
-
 String map() {
   return """
     <html>
